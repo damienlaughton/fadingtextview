aa0617b226937db10aadd9f5963023e51b0570c4
Damien Laughton <damien@imano.com>
2011-12-30 15:47:54 +0000

    Initial Commit

---
+++ fadingtextview/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  fadingtextview
+//
+//  Created by Damien Laughton on 30/12/2011.
+//  Copyright (c) 2011 Mobilology Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ fadingtextview/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fadingtextview
+//
+//  Created by Damien Laughton on 30/12/2011.
+//  Copyright (c) 2011 Mobilology Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
